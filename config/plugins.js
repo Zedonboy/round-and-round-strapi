39ec0bb6d29515bad1cfb761641489901b5c5925
Declan Nnadozie <zedonbiz@gmail.com>
2022-11-08 01:53:47 +0100

    update

---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+const crypto = require("crypto")
+module.exports = ({ env }) => ({
+    // ...
+    'users-permissions': {
+      config: {
+        jwtSecret: crypto.randomBytes(16).toString("base64"),
+        jwt: {
+          expiresIn: '1d',
+        },
+      },
+    },
+    // ...
+  });
